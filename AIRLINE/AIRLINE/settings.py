8ed7e946fc550c100cb9fddfcc32cf95491520ea
Rozan <akhmadrozanaditya@gmail.com>
2024-09-22 15:13:18 +0700

    add class on models.py
    not migrated yet

--- AIRLINE/AIRLINE/settings.py
+++ AIRLINE/AIRLINE/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fligths',
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
