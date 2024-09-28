f5939d7f96c83073a7a170d43bfefe508e134658
Rozan <akhmadrozanaditya@gmail.com>
2024-09-29 01:10:37 +0700

    create infinity scroll post page
    using javascript

--- scroll/posts/urls.py
+++ scroll/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.posts, name="posts")
+    path("posts", views.posts, name="posts")
 ]
--- scroll/posts/views.py
+++ scroll/posts/views.py
@@ -1,26 +1,24 @@
 import time
-from django.shortcuts import render
-from django.http import JsonResponse
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# create your views here/
 def index(request):
     return render(request, "posts/index.html")
 
-# Function to generate the posts
 def posts(request):
-    # Get start and end points
+
+    #Get start and end poinst
     start = int(request.GET.get("start") or 0)
     end = int(request.GET.get("end") or (start + 9))
 
-    # Generate list of posts
+    #generate list of post
     data = []
-    for i in range(start, end + 1):
+    for i in range(start, end +1):
         data.append(f"Post #{i}")
 
-    # Artificially delay speed of response
+    #Artificially delay speed of response
     time.sleep(1)
 
-    # Return list of posts
-    return JsonResponse({
-        "posts": data  # Return the list of posts in the JSON response
-    })
+    return JsonResponse({"posts": data})
