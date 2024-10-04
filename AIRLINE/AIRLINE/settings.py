9617fddde9974b970f8032a047d6d8b493df3b84
Rozan <akhmadrozanaditya@gmail.com>
2024-10-04 15:56:31 +0700

    move singlepage1 to new project
    create singlepage1 project and remove it from airline project

--- AIRLINE/AIRLINE/settings.py
+++ AIRLINE/AIRLINE/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'singlepage1',
     'flights',
     'users',
     'django.contrib.admin',
--- singlepage/singlepage/urls.py
+++ singlepage/singlepage/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AIRLINE project.
+URL configuration for singlepage project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flights/", include("flights.urls")),
-    path("users/", include("users.urls")),
+    path("singlepage1/", include("singlepage1.urls")),
+    path("", include("singlepage1.urls"))
 ]
